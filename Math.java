a5510ec92c724a7be912cf5110d6d265cbbb1a17
Saurabh z <101171252+saur1999@users.noreply.github.com>
2022-03-15 12:07:40 +0530

    NEW FOLDER

---
+++ Math.java
@@ -0,0 +1,12 @@
+class Math{
+static int mul(int n1,int n2,int n3)
+{
+int result =n1*n2*n3;
+System.out.println("Ans  = "+result);
+return result;
+}
+public static void main(String[] args){
+//System.out.println(add(10,10));
+mul(10,12,13);
+}
+}
