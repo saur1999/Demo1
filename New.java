2e0b6f88160ade20fc564e05cd4ef22387eea11d
Saurabh z <101171252+saur1999@users.noreply.github.com>
2022-03-15 12:09:27 +0530

    NEW JAVA

---
+++ New.java
@@ -0,0 +1,13 @@
+package org.cdacnoida.course.hello;
+
+
+
+public class New {
+
+	public static void main(String[] args) {
+		int x = 10;
+		System.out.println(x);
+         System.out.println("hello bhai");
+	}
+
+}
